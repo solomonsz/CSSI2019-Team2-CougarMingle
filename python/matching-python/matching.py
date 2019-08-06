fdc00d04f76efab0a34c02df514f4cf7321378ea
Ruiqi Liu <liu060@csusm.edu>
2019-08-06 10:48:34 -0700

    Moving everything

--- python/matching-python/matching.py
+++ python/matching-python/matching.py
@@ -1,35 +1,28 @@
 import webapp2
 import jinja2
 import os
-
-
+from models import *
+from google.appengine.ext import ndb
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-jack = "Jack"
+
 # the handler section
-class EnterInfoHandler(webapp2.RequestHandler):
+class ChatPage(webapp2.RequestHandler):
     def get(self):  # for a get request
         welcome_template = the_jinja_env.get_template('template/matchPage.html')
+        name = first_name.query().fetch()
         a_variable_dict = {
-            "name": jack
+            "name": name
             # "adjective": "amazing"
         }
         self.response.write(welcome_template.render(a_variable_dict))
-#
-# class ShowMemeHandler(webapp2.RequestHandler):
-#     def get(self):
-#         results_template = the_jinja_env.get_template('templates/results.html')
-#         the_variable_dict = {
-#             "line1": "If Cinderella's shoe was a perfect fit",
-#             "line2": "Why did it fall off?",
-#             "img_url": "https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg"
-#         }
-#         self.response.write(results_template.render(the_variable_dict))
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', EnterInfoHandler)
+    ('/',ChatPage)
     # ('/memeresult', ShowMemeHandler)
 ], debug=True)
