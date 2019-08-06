b2cd9b9989d2bff74b7a46dfcbb4831a4d875722
Gabriela Rojas <rojas124@csusm.edu>
2019-08-06 09:32:20 -0700

    deleted files and created userPage

--- models.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import ndb
-
-class CsusmUser(ndb.Model):
-  user_first_name = ndb.StringProperty()
-  user_second_name = ndb.StringProperty()
-  u_user_email = ndb.StringProperty(indexed=True)
